4eaff604cdc46070599d97d1416a4e0a6240962c
xzzgwzh <133201531+xzzgwzh@users.noreply.github.com>
2023-09-02 14:46:56 +0000

    1 Updated By Github Actions

--- js/jquery.litenav.js
+++ js/jquery.litenav.js
@@ -1,26 +1,35 @@
-; (function ($) {
+(function ($) {
     $.fn.extend({
         "liteNav": function (t) {
-            var $this = $(this), i = 0, $pics = $('#NewsPic'), autoChange = function () {
-                var $currentPic = $pics.find('a:eq(' + (i + 1 === 7 ? 0 : i + 1) + ')');
-                $currentPic.css({
-                    visibility: 'visible',
-                    display: 'block'
-                }).siblings('a').css({
-                    visibility: 'hidden',
-                    display: 'none'
-                });
-                $pics.find('.Nav>span:contains(' + (i + 2 > 7 ? 7 - i : i + 2) + ')').attr('class', 'Cur').siblings('span').attr('class', 'Normal');
-                $('#NewsPicTxt').html('<a target="_blank" href="' + $currentPic[0].href + '">' + $currentPic.find('img').attr('title') + '</a>');
-                i = i + 1 === 7 ? 0 : i + 1;
-            }, st = setInterval(autoChange, t || 2000);
+            var $this = $(this),
+                i = 0,
+                $pics = $('#NewsPic'),
+                numImages = $pics.find('a').length, // 获取图片的总数
+                autoChange = function () {
+                    var $currentPic = $pics.find('a:eq(' + (i + 1 === numImages ? 0 : i + 1) + ')');
+                    $currentPic.css({
+                        visibility: 'visible',
+                        display: 'block'
+                    }).siblings('a').css({
+                        visibility: 'hidden',
+                        display: 'none'
+                    });
+                    $pics.find('.Nav>span:contains(' + (i + 2 > numImages ? numImages - i : i + 2) + ')').attr('class', 'Cur').siblings('span').attr('class', 'Normal');
+                    $('#NewsPictxt').html('<a target="_blank" href="' + $currentPic[0].href + '">' + $currentPic.find('img').attr('title') + '</a>');
+                    i = i + 1 === numImages ? 0 : i + 1;
+                },
+                st = setInterval(autoChange, t || 2000);
+
             $this.hover(function () {
                 clearInterval(st);
-            }, function () { st = setInterval(autoChange, t || 2000) });
+            }, function () {
+                st = setInterval(autoChange, t || 2000);
+            });
+
             $pics.find('.Nav>span').click(function () {
                 i = parseInt($(this).text(), 10) - 2;
                 autoChange();
             });
         }
     });
-}(jQuery));
+})(jQuery);
